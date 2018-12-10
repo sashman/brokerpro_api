b9fd0fc92b68da8fd9498ccb91a7cf3de9ecb5d4
sashman <sashman90@gmail.com>
2018-12-10 21:55:04 +0000

    Prettier and husky

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const { GraphQLServer } = require("graphql-yoga")
-const { prisma } = require('./generated/prisma-client')
+const { prisma } = require("./generated/prisma-client")
 const morgan = require("morgan")
-const PORT = 4000;
+const PORT = 4000
 
 const resolvers = {
   Query: {
     policies: prisma.policies,
-    policy:  (_, { id }) => prisma.policies.where({ id }),
+    policy: (_, { id }) => prisma.policies.where({ id }),
     customers: prisma.customers,
-    customer:  (_, { id }) => prisma.customers.where({ id })
+    customer: (_, { id }) => prisma.customers.where({ id })
   },
   Mutation: {
-    // createPolicy: (_, { id }) => prisma.policies.where({ id }),
     createCustomer: (_, { customer }) => prisma.createCustomer(customer),
     updateCustomer: (_, { customer, id }) =>
       prisma.updateCustomer({ data: customer, where: { id } })
@@ -20,11 +19,11 @@ const resolvers = {
 
 const server = new GraphQLServer({
   port: PORT || 4000,
-  typeDefs: './schema.graphql',
+  typeDefs: "./schema.graphql",
   resolvers,
   context: {
     prisma
-  },
+  }
 })
 
 server.express.use(morgan("combined"))
